9f8ae4e8254e57cd065287c15346d96e59ce1f83
Tucker <tuckerhoog@tutanota.com>
2024-02-23 10:47:17 -0500

    update list of players online every 3 seconds

--- src/pages/tools/Adventuers.jsx
+++ src/pages/tools/Adventuers.jsx
@@ -1,4 +1,13 @@
-import { For, createSignal, createResource, Show } from 'solid-js'
+import {
+  For,
+  createSignal,
+  createResource,
+  Show,
+  onCleanup,
+  onMount,
+  startTransition,
+  Suspense,
+} from 'solid-js'
 import { Select } from '@thisbeyond/solid-select'
 import { createForm } from '@felte/solid'
 import { Adventurer } from '../../components/adventurer/Adventurer'
@@ -12,10 +21,18 @@ export function Adventurers() {
   const [selectedJob, setSelectedJob] = createSignal()
   const [fetchedByName, setFetchedByName] = createSignal()
 
-  const [fetchedOnline] = createResource(
+  const [fetchedOnline, { refetch }] = createResource(
     () => [selectedZone(), selectedJob()],
     fetchOnline,
   )
+
+  onMount(() => {
+    const timer = setInterval(() => startTransition(refetch), 3000)
+
+    onCleanup(() => clearInterval(timer))
+  })
+
+  onCleanup(() => clearInterval(timer))
 
   const { form, errors } = createForm({
     onSubmit: async (values) => {
@@ -52,74 +69,103 @@ export function Adventurers() {
   })
 
   return (
-    <div class='adventurersContainer'>
-      <form class='adventurerForm' use:form>
-        <label class='adventurerLabel'>
-          Search By Name
-          <input
-            class='adventurerInput'
-            type='text'
-            placeholder='Find An Adventurer'
-            name='nameSearch'
-          />
-        </label>
-        <div class='adventurersButtonContainer'>
-          <button
-            type='reset'
-            onClick={() => {
-              setSelectedZone(undefined)
-              setSelectedJob(undefined)
-              setFetchedByName(undefined)
-            }}
+    <Suspense>
+      <div class='adventurersContainer'>
+        <form class='adventurerForm' use:form>
+          <label class='adventurerLabel'>
+            Search By Name
+            <input
+              class='adventurerInput'
+              type='text'
+              placeholder='Find An Adventurer'
+              name='nameSearch'
+            />
+          </label>
+          <div class='adventurersButtonContainer'>
+            <button
+              type='reset'
+              onClick={() => {
+                setSelectedZone(undefined)
+                setSelectedJob(undefined)
+                setFetchedByName(undefined)
+              }}
+            >
+              Reset
+            </button>
+            <button type='submit'>Search</button>
+            <Show
+              when={errors().serverError || fetchedOnline()?.errors?.errors}
+            >
+              <p>
+                {errors().serverError} {fetchedOnline()?.errors?.errors}
+              </p>
+            </Show>
+          </div>
+          <Show
+            when={
+              fetchedByName() === undefined || fetchedByName()?.length === 0
+            }
           >
-            Reset
-          </button>
-          <button type='submit'>Search</button>
-          <Show when={errors().serverError || fetchedOnline()?.errors?.errors}>
-            <p>
-              {errors().serverError} {fetchedOnline()?.errors?.errors}
-            </p>
+            <div class='zoneSelectContainer'>
+              <label class='adventurerLabel' for='zoneSelect'>
+                Filter Online By Zone
+              </label>
+              <Select
+                onChange={(e) => setSelectedZone(e)}
+                class='custom'
+                options={fetchedOnline()?.onlineZones}
+                format={(item) => zones[item]}
+                placeholder='Search A Zone'
+                id='zoneSelect'
+              />
+            </div>
+            <div class='jobSelectContainer'>
+              <label class='adventurerLabel' for='jobSelect'>
+                Filter Online By Job
+              </label>
+              <Select
+                onChange={(e) => setSelectedJob(e)}
+                class='custom'
+                options={fetchedOnline()?.onlineJobs}
+                format={(item) => jobConverter(item)}
+                placeholder='Search A Job'
+                id='jobSelect'
+              />
+            </div>
           </Show>
-        </div>
-        <Show
-          when={fetchedByName() === undefined || fetchedByName()?.length === 0}
-        >
-          <div class='zoneSelectContainer'>
-            <label class='adventurerLabel' for='zoneSelect'>
-              Filter Online By Zone
-            </label>
-            <Select
-              onChange={(e) => setSelectedZone(e)}
-              class='custom'
-              options={fetchedOnline()?.onlineZones}
-              format={(item) => zones[item]}
-              placeholder='Search A Zone'
-              id='zoneSelect'
-            />
-          </div>
-          <div class='jobSelectContainer'>
-            <label class='adventurerLabel' for='jobSelect'>
-              Filter Online By Job
-            </label>
-            <Select
-              onChange={(e) => setSelectedJob(e)}
-              class='custom'
-              options={fetchedOnline()?.onlineJobs}
-              format={(item) => jobConverter(item)}
-              placeholder='Search A Job'
-              id='jobSelect'
-            />
-          </div>
-        </Show>
-      </form>
-      <div class='resultsContainer'>
-        <Show
-          when={fetchedByName() === undefined || fetchedByName()?.length === 0}
-          fallback={
+        </form>
+        <div class='resultsContainer'>
+          <Show
+            when={
+              fetchedByName() === undefined || fetchedByName()?.length === 0
+            }
+            fallback={
+              <section>
+                <h2 class='onlineTitle'>Search Results</h2>
+                <ul>
+                  <For each={fetchedByName()}>
+                    {(adventurer) => (
+                      <li
+                        class='adventurerRow'
+                        onClick={() =>
+                          window.localStorage.setItem(
+                            'detailsName',
+                            adventurer.charname,
+                          )
+                        }
+                      >
+                        <Adventurer {...adventurer} />
+                      </li>
+                    )}
+                  </For>
+                </ul>
+              </section>
+            }
+          >
             <section>
-              <h2 class='onlineTitle'>Search Results</h2>
+              <h2 class='onlineTitle'>Online</h2>
               <ul>
-                <For each={fetchedByName()}>
+                <For each={fetchedOnline()?.adventurersOnline}>
                   {(adventurer) => (
                     <li
                       class='adventurerRow'
@@ -136,30 +182,9 @@ export function Adventurers() {
                 </For>
               </ul>
             </section>
-          }
-        >
-          <section>
-            <h2 class='onlineTitle'>Online</h2>
-            <ul>
-              <For each={fetchedOnline()?.adventurersOnline}>
-                {(adventurer) => (
-                  <li
-                    class='adventurerRow'
-                    onClick={() =>
-                      window.localStorage.setItem(
-                        'detailsName',
-                        adventurer.charname,
-                      )
-                    }
-                  >
-                    <Adventurer {...adventurer} />
-                  </li>
-                )}
-              </For>
-            </ul>
-          </section>
-        </Show>
+          </Show>
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
